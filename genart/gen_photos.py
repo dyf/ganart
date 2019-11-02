a094c060ab1d25b378e7af7153f8b179e562a247
dyf <dyf@fake.email>
2019-11-02 05:58:32 -0700

    photos too big

--- genart/gen_photos.py
+++ genart/gen_photos.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 from pathlib import Path
 import re
 
@@ -8,6 +9,8 @@ class ScaledImageDataLoader:
 
         self.small_images = list(self.basedir.glob("small/img-small*"))
         self.large_images = list(self.basedir.glob("large/img-large*"))
+
+        self.small_shape = (128,128,3)
         
         small_idxs = set()
         for im in self.small_images:
@@ -32,15 +35,38 @@ class ScaledImageDataLoader:
         idx = self.all_idxs[i]
 
         f = tf.io.read_file(str(self.basedir / 'small' / f'img-small-{idx}.jpg'))
-        small_image = tf.image.decode_jpeg(f)
+        small_image = (tf.cast(tf.image.decode_jpeg(f), tf.float32) / 127.5) - 1.0
         f = tf.io.read_file(str(self.basedir / 'large' / f'img-large-{idx}.jpg'))
-        large_image = tf.image.decode_jpeg(f)
+        large_image = (tf.cast(tf.image.decode_jpeg(f), tf.float32) / 127.5) - 1.0
 
-        return self.square_image(small_image), self.square_image(large_image)
+        small_image = self.square_image(small_image)
+        large_image = self.square_image(large_image)
+
+        if small_image.shape[0] != 200:
+            raise ValueError(f'image {i} has a weird shape: {str(small_image.shape)}')
+        elif large_image.shape[0] != 800:
+            raise ValueError(f'image {i} has a weird shape: {str(large_image.shape)}')
+
+        sf = large_image.shape[0] // small_image.shape[0]
+        
+        start_sm = np.random.randint(0, small_image.shape[0] - self.small_shape[0], 2)
+        start_lg = start_sm * sf
+        large_shape = (self.small_shape[0]*sf, self.small_shape[1]*sf, 3)
+        return (
+            small_image[start_sm[0]:start_sm[0]+self.small_shape[0],
+                        start_sm[1]:start_sm[1]+self.small_shape[1],:],
+            large_image[start_lg[0]:start_lg[0]+large_shape[0],
+                        start_lg[1]:start_lg[1]+large_shape[1],:]
+        )
 
     def __getitem__(self, idx):
         if isinstance(idx, slice):
-            imgs = [ self.load_image(i) for i in range(*idx.indices(len(self))) ]
+            imgs = []
+            for i in range(*idx.indices(len(self))):
+                try:
+                    imgs.append(self.load_image(i))
+                except ValueError as e:
+                    print(e)
             return tf.stack([img[0] for img in imgs]), tf.stack([img[1] for img in imgs])
         else:
             sm, lg = self.load_image(idx)
@@ -52,6 +78,7 @@ if __name__ == '__main__':
     print(sm.shape)
     print(lg.shape)
     #print(imdl.small_images)
+
 
 
 
