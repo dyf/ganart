da874678cf509be2a1f0528ddcb62f74bb7cff0d
dyf <davidf@alleninstitute.org>
2020-06-23 23:04:50 -0700

    style-aware model fixup

--- genart/tf/chinese/model.py
+++ genart/tf/chinese/model.py
@@ -55,48 +55,51 @@ def build_gan(latent_size=100):
 
     return generator, discriminator
 
-def build_class_gan(latent_size=100):
+def build_class_gan(latent_size=100, num_fonts=10):
     
     latent_input = Input(shape=(latent_size,))
-    class_input = Input(shape=(1,))
+    ts_input = Input(shape=(3,))
+    font_input = Input(shape=(num_fonts+1,))
 
-    layer_1_input = Concatenate()([latent_input, class_input])
+    layer_1_input = Concatenate()([latent_input, ts_input, font_input])
 
     layer_1 =  Sequential([
-        Dense(7*7*256, use_bias=False),
+        Dense(8*8*256, use_bias=False),
         BatchNormalization(),
         LeakyReLU(),
-        Reshape((7,7,256))
-    ])
+        Reshape((8,8,256)),
 
-    layer_1_output = layer_1(layer_1_input)
-    layer_2_input = Concatenate()([layer_1_output, class_input])
-
-    layer_2 = Sequential([
-        Conv2DTranspose(128, (5,5), strides=(1,1), padding='same', use_bias=False),
+        Conv2DTranspose(256, (5,5), strides=(1,1), padding='same', use_bias=False),
         BatchNormalization(),
         LeakyReLU(),
-    ])
 
-    layer_2_output = layer_2(layer_2_input)
-    layer_2_input = Concatenate()([layer_2_output, class_input])
+        Conv2DTranspose(128, (5,5), strides=(2,2), padding='same', use_bias=False),
+        BatchNormalization(),
+        LeakyReLU(),
 
-    layer_3 = Sequential([
         Conv2DTranspose(64, (5,5), strides=(2,2), padding='same', use_bias=False),
         BatchNormalization(),
-        LeakyReLU()
+        LeakyReLU(),
+
+        Conv2DTranspose(64, (5,5), strides=(2,2), padding='same', use_bias=False),
+        BatchNormalization(),
+        LeakyReLU(),
+
+        Conv2DTranspose(1, (5,5), strides=(2,2), padding='same', use_bias=False, activation='tanh')
     ])
 
-    layer_3_output = layer_3(layer_3_input)
-    layer_4_input = Concatenate()([layer_3_output, class_input])
+    generator_output = layer_1(layer_1_input)
+    
+    generator = Model([latent_input, ts_input, font_input], generator_output)
 
-    layer_4 = Conv2DTranspose(1, (5,5), strides=(2,2), padding='same', use_bias=False, activation='tanh')
-    generator_output = layer_4(layer_4_input)
+    discriminator_input = Input(shape=(128,128,1))
+    
+    discriminator_conv = Sequential([
+        Conv2D(64, (5, 5), strides=(2, 2), padding='same'), 
+        LeakyReLU(),
+        Dropout(0.3),
 
-    generator = Model([latent_input, class_input], generator_output)
-
-    discriminator = Sequential([
-        Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=[28, 28, 1]),
+        Conv2D(64, (5, 5), strides=(2, 2), padding='same'), 
         LeakyReLU(),
         Dropout(0.3),
         
@@ -104,16 +107,32 @@ def build_class_gan(latent_size=100):
         LeakyReLU(),
         Dropout(0.3),
 
-        Flatten(),
-        Dense(3, activation='sigmoid')
+        Conv2D(256, (5, 5), strides=(2, 2), padding='same'),
+        LeakyReLU(),
+        Dropout(0.3),
+
+        Flatten()
     ])
+
+    ts_classifier = Dense(3, activation='sigmoid')
+    font_classifier = Dense(num_fonts, activation='sigmoid')
+
+    dco = discriminator_conv(discriminator_input)
+
+    discriminator = Model(discriminator_input, [ ts_classifier(dco), font_classifier(dco) ])
 
     return generator, discriminator
 
 if __name__ == "__main__":
-    seed = tf.random.normal((1,100))
-    gen, disc = build_gan(100)
-    gpred = gen.predict(seed)
+    batch_size = 2
+    latent_size = 100
+    num_fonts = 10
+    
+    seed = tf.random.normal((batch_size,latent_size))
+    classes = tf.random.uniform((batch_size,), minval=0, maxval=3, dtype=tf.dtypes.int32)
+    fonts = tf.random.uniform((batch_size,), minval=0, maxval=num_fonts+1, dtype=tf.dtypes.int32)
 
-    dpred = disc.predict(gpred)
-    print(gpred.shape, dpred.shape)
+    gen, disc = build_class_gan(latent_size, num_fonts)
+    gpred = gen.predict([seed, tf.one_hot(classes, depth=3), tf.one_hot(fonts, depth=num_fonts+1)])
+    [dpred_ts, dpred_font] = disc.predict(gpred)
+    print(gpred.shape, dpred_ts.shape, dpred_font.shape)
